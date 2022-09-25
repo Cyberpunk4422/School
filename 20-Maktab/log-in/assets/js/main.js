94a9666db4cefaa37f6a6bdbb7e523f7c0b8c184
Cyberpunk4422 <112374485+Cyberpunk4422@users.noreply.github.com>
2022-09-25 06:34:28 +0500

    Add files via upload

---
+++ 20-Maktab/log-in/assets/js/main.js
@@ -0,0 +1,5 @@
+/*===== LOGIN SHOW and HIDDEN =====*/
+const signUp = document.getElementById('sign-up'),
+    signIn = document.getElementById('sign-in'),
+    loginIn = document.getElementById('login-in'),
+    loginUp = document.getElementById('login-up')
